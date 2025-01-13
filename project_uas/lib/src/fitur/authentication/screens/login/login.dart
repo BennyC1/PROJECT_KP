09e602fe84e69ce480b11eab15d84eb092725630
BennyC1 <160693849+BennyC1@users.noreply.github.com>
2025-01-14 02:43:17 +0700

    update

--- project_uas/lib/src/fitur/authentication/screens/login/login.dart
+++ project_uas/lib/src/fitur/authentication/screens/login/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:project_uas/src/utils/constants/sized.dart';
+
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -7,7 +9,14 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: SingleChildScrollView(
-        child: Padding(padding: EdgeInsets.only()),
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: BSize.appBarHeight,
+            left: BSize.defaultSpace,
+            bottom: BSize.defaultSpace,
+            right: BSize.defaultSpace,
+          ),
+        ),
       ),
     );
   }
