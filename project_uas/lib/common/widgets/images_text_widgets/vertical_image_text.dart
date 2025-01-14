c165f700f4e560338e350e817c636ec9951b0b21
BennyC1 <160693849+BennyC1@users.noreply.github.com>
2025-01-15 03:29:43 +0700

    update

--- project_uas/lib/common/widgets/images_text_widgets/vertical_image_text.dart
+++ project_uas/lib/common/widgets/images_text_widgets/vertical_image_text.dart
@@ -38,7 +38,7 @@ class BVerticalImageText extends StatelessWidget {
                 borderRadius: BorderRadius.circular(100),
               ),
               child: Center(
-                child: Image(image: AssetImage(BImages.shoeIcon), fit: BoxFit.cover, color: dark ? BColors.light : BColors.dark),
+                child: Image(image: AssetImage(BImages.shoeIcon), fit: BoxFit.cover, color: dark ? BColors.dark : BColors.dark),
               ),
             ),
             SizedBox(height: BSize.spaceBtwItems / 2),
